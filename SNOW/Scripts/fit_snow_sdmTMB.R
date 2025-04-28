4ceb098484b06d4843ac970f282040ea164755e6
Emily Ryznar <emily.ryznar@nmfs.local>
2025-04-28 13:22:06 -0800

    fit snow models

---
+++ SNOW/Scripts/fit_snow_sdmTMB.R
@@ -0,0 +1,144 @@
+### PURPOSE ----------------------------------------------------------------------
+# To generate model-based indices of biomass for EBS snow crab for males >=95mm
+# and mature females. Time range is 1980-present.
+
+# Author: Emily Ryznar
+
+# TO DOs:
+# 1)
+
+### LOAD LIBRARIES/DATA -----------------------------------------------------------
+source("./SNOW/Scripts/load_libs_functions.R")
+
+### LOAD FUNCTION -----------------------------------------------------------------
+fit_models <- function(data, category, years, dist, knots, region){
+  
+  # Filter data by params
+  if(region != "All"){
+    data %>%
+      filter(category == category, year %in% years, region == region) %>%
+      mutate(year_fac = as.factor(year)) -> data2
+  
+  }else{
+    data %>%
+      filter(category == category, year %in% years) %>%
+      mutate(year_fac = as.factor(year)) -> data2
+  }
+  
+  # Set spatiotemporal estimator
+  if(region != "EBS"){
+    sptmp <- "ar1"
+  }else{
+    sptmp <- "iid"
+  }
+   
+  # Make mesh
+  mesh2 <- make_mesh(data2, c("lon","lat"), n_knots = knots, type = "kmeans")
+  
+  if(dist == "DG"){
+    # Fit models
+    print("Fitting biomass model")
+    bio <- sdmTMB(cpue_kg_km ~ 0 + year_fac, #the 0 is there so there is a factor predictor for each time slice
+                  spatial = "on",
+                  spatiotemporal = sptmp,
+                  mesh = mesh2,
+                  family = delta_gamma(type = "poisson-link"),
+                  time = "year",
+                  anisotropy = TRUE,
+                  data = data2)
+    
+  } else if(dist == "TW"){
+    # Fit models
+    print("Fitting biomass model")
+    bio <- sdmTMB(cpue_kg_km ~ 0 + year_fac, #the 0 is there so there is a factor predictor for each time slice
+                  spatial = "on",
+                  spatiotemporal = sptmp,
+                  mesh = mesh2,
+                  family = tweedie(link = "log"),
+                  time = "year",
+                  anisotropy = TRUE,
+                  data = data2)
+  } else{
+    # Fit models
+    print("Fitting biomass model")
+    bio <- sdmTMB(cpue_kg_km ~ 0 + year_fac, #the 0 is there so there is a factor predictor for each time slice
+                  spatial = "on",
+                  spatiotemporal = sptmp,
+                  mesh = mesh2,
+                  family = delta_lognormal(),
+                  time = "year",
+                  anisotropy = TRUE,
+                  data = data2)
+    
+  }
+  
+  
+  saveRDS(bio, paste0(dir, "Models/snow_", region, "_", category, "_", knots, "_", dist, "_bioTMB.rda"))
+  
+  return(list(bioTMB = bio, mesh = mesh2))
+  
+}
+
+### SNOW EBS ------------------------------------------------------------------
+years <- c(1980:2019, 2021:2024)
+
+# Filter pred_grid by stock, transform to UTM, replicate by number of years
+ebs_grid2 <- ebs_grid %>%
+  st_as_sf(., coords = c("X", "Y"), crs = "+proj=utm +zone=2") %>%
+  st_transform(., crs = "+proj=utm +zone=2") %>%
+  cbind(st_coordinates(.)) %>%
+  as.data.frame(.) %>%
+  dplyr::select(area_km2, X, Y) %>%
+  replicate_df(., "year", years) %>%
+  mutate(X = X/1000, Y = Y/1000) %>%
+  rename(lon = X, lat = Y)
+
+## Mature female EBS DG -----
+data <- snow.matfem.cpue
+category <- "Mature female"
+region <- "EBS"
+dist <- "DG"
+
+# Fit models
+fit_models(data, category, years, dist, knots = 50, region) -> ebs.mf.50
+fit_models(data, category, years, dist, knots = 120, region) -> ebs.mf.120
+
+## Mature female EBS TW -----
+data <- snow.matfem.cpue
+category <- "Mature female"
+region <- "EBS"
+dist <- "TW"
+
+# Fit models
+fit_models(data, category, years, dist, knots = 50, region) -> ebs.mf.50
+fit_models(data, category, years, dist, knots = 120, region) -> ebs.mf.120 # doesn't fit!!!
+
+## Male EBS DG -----
+data <- snow.male95.cpue 
+category <- "Male95"
+region <- "EBS"
+dist <- "DG"
+
+# Fit models
+fit_models(data, category, years, dist, knots = 50, region) -> ebs.m.50
+fit_models(data, category, years, dist, knots = 120, region) -> ebs.m.120
+
+## Male EBS TW -----
+data <- snow.male95.cpue 
+category <- "Male95"
+region <- "EBS"
+dist <- "TW"
+
+# Fit models
+fit_models(data, category, years, dist, knots = 50, region) -> ebs.m.50
+fit_models(data, category, years, dist, knots = 120, region) -> ebs.m.120
+
+
+# # Predict and get index
+# abund.mod <- readRDS("./BAIRDI/Models/bairdi_Male_TannerW_pre-1988_50_abundTMB.rda")
+# bio.mod <- readRDS("./BAIRDI/Models/bairdi_Male_TannerW_pre-1988_50_bioTMB.rda")
+# 
+# abund.mod <- readRDS("./BAIRDI/Models/bairdi_Male_TannerW_pre-1988_120_abundTMB.rda")
+# bio.mod <- readRDS("./BAIRDI/Models/bairdi_Male_TannerW_pre-1988_120_bioTMB.rda")
+
+#predict_and_getindex(newdat, abund.mod, bio.mod, matsex, stock, years, period, knots = 120) ->  ind.Wmale1
